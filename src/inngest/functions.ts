40e398ecafed94ea73f02328007781bbd07ef0e6
desaiHarsh08 <desaiharsh183@gmail.com>
2025-07-11 00:00:51 +0530

    06: e2b-sandboxes

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,16 @@
 import { inngest } from "./client";
+import { Sandbox } from "@e2b/code-interpreter";
 import { createAgent, gemini } from "@inngest/agent-kit";
+import { getSandbox } from "./utils";
 
 export const helloWorld = inngest.createFunction(
     { id: "hello-world" },
     { event: "test/hello.world" },
-    async ({ event }) => {
+    async ({ event, step }) => {
+        const sandboxId = await step.run("get-sandbox-id", async () => {
+            const { sandboxId } = await Sandbox.create("my-vibe");
+            return sandboxId;
+        });
         const codeWriterAgent = createAgent({
             name: 'code-agent',
             system:
@@ -16,8 +22,13 @@ export const helloWorld = inngest.createFunction(
             `Write the following snippet: ${event.data.value}`,
         );
         console.log(output);
-        // [{ role: 'assistant', content: 'function removeUnecessaryWhitespace(...' }]
 
-        return { output };
+        const sandboxUrl = await step.run("get-sandbox-url", async () => {
+            const sandbox = await getSandbox(sandboxId);
+            const host = sandbox.getHost(3000);
+            return `https://${host}`
+        });
+
+        return { output, sandboxUrl };
     },
 );
